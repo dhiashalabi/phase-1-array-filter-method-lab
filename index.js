37b3aaadca0deb54b84d5fb88d3db71e8be28b8e
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-13 08:59:11 +0300

    feat: completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.startsWith(name);
+  });
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name === name;
+  });
+}
